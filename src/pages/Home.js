cc03fe3d26553a3d0c364d281e66fe9e35f8fe4a
bluecoder01 <onesixteenj@gmail.com>
2023-10-05 12:09:54 +0100

    -

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,12 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Nav from '../components/Nav'
 import Footer from '../components/Footer'
 
 
 export default function Home() {
+
+  const [words, setWords] = useState("");
+
   return (
     <div>
         <Nav isUserLoggedIn={true} />
@@ -12,7 +15,9 @@ export default function Home() {
             <p>Take your financial life online. Your Easybank account will be a one-stop-shop 
             for spending, saving, budgeting, investing, and much more.</p>
         </div>
-        
+        <input type="text" value={words} onChange={(e) => {
+          console.log(words);
+          setWords(e.target.value)}}/>
         <Footer />
     </div>
   )
